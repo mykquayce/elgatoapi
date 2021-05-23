df0dbfbcebcc6630e26fcf91357f82511c3ed921
Bob H <mail@bob.codes>
2021-05-23 17:12:09 +0100

    adding versioning

--- build.sh
+++ build.sh
@@ -1,11 +1,23 @@
 #! /bin/bash
 
+
 # pull base images
 docker pull mcr.microsoft.com/dotnet/aspnet:6.0
 docker pull mcr.microsoft.com/dotnet/sdk:6.0
 
+
 # build
-docker build --tag eassbhhtgu/elgatoapi:latest .
+version=$(cat ./ElgatoApi.WebApplication/*.csproj | \
+	grep --ignore-case --only-matching --perl-regex "<version>.+?</version>" | \
+	sed --quiet --regexp-extended 's/<(.+?)>(.*)<\/\1>/\2/p')
+
+image=eassbhhtgu/elgatoapi
+tag1=$image:$version
+tag2=$image:latest
+
+docker build --tag $tag1 --tag $tag2 .
+
 
 # push
-docker push eassbhhtgu/elgatoapi:latest
+docker push $tag1
+docker push $tag2
